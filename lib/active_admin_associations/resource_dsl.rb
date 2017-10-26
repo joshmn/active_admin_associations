279b18d075a04ba911e4395a5d8f0e585b1fcf5d
Josh Brody <josh@josh.mn>
2017-10-26 06:55:34 -0500

    lingering

--- lib/active_admin_associations/resource_dsl.rb
+++ lib/active_admin_associations/resource_dsl.rb
@@ -14,7 +14,7 @@ module ActiveAdminAssociations
       config.add_action_item("Associations", :except => [:index, :new]) do
         dropdown_menu "Associations" do
           resource_associations.each do |association|
-            item association.label, [current_namespace, association.name, "q[#{resource.model_name.param_key}_id_equals]".to_sym => resource.to_param] }
+            item association.label, [current_namespace, association.name, "q[#{resource.model_name.param_key}_id_equals]".to_sym => resource.to_param]
           end
         end
       end
