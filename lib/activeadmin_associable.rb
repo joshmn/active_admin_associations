9af7c2b78dabdbc96e14788db84d83b548db29f4
Josh Brody <josh@josh.mn>
2017-11-04 14:58:45 -0500

    Update activeadmin_associable.rb

--- lib/activeadmin_associable.rb
+++ lib/activeadmin_associable.rb
@@ -1,7 +1,7 @@
 require 'active_admin'
 require 'activeadmin_associable/associable'
 require 'activeadmin_associable/resource_dsl'
-require 'activeadmin_associable/active_admin/resource/associations'
+require 'activeadmin_associable/active_admin/resource/associables'
 
 require 'activeadmin_associable/version'
 
@@ -10,7 +10,7 @@ module ActiveAdminAssociable
     class Engine < ::Rails::Engine
       config.after_initialize do
         ::ActiveAdmin::ResourceDSL.send(:include, ActiveAdminAssociable::ResourceDSL)
-        ::ActiveAdmin::Resource.send(:include, ActiveAdminAssociable::ActiveAdmin::Resource::Associations)
+        ::ActiveAdmin::Resource.send(:include, ActiveAdminAssociable::ActiveAdmin::Resource::Associables)
       end
     end
   end
