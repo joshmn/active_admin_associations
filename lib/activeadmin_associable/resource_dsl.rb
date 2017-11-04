6a95325b266c8559a7e938d2545e6fae3aa6b2eb
Josh Brody <josh@josh.mn>
2017-11-04 15:00:04 -0500

    Update resource_dsl.rb

--- lib/activeadmin_associable/resource_dsl.rb
+++ lib/activeadmin_associable/resource_dsl.rb
@@ -7,12 +7,12 @@ module ActiveAdminAssociable
       config.associables << ActiveAdminAssociable::Associable.new(name, options)
     end
 
-    def associable_menu(name, options = {})
+    def associable_menu(name = "Associables", options = {})
       return nil if config.associables.empty?
       resource_associations = config.associables
       current_namespace = config.namespace.name
-      config.add_action_item("Associates", :except => [:index, :new]) do
-        dropdown_menu "Associates" do
+      config.add_action_item(name, :except => [:index, :new]) do
+        dropdown_menu name do
           resource_associations.each do |association|
             item association.label, [current_namespace, association.name, "q[#{resource.model_name.param_key}_id_equals]".to_sym => resource.to_param]
           end
